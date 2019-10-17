01e40881585fe3b43bae2081c684236ece3b2929
Miljan Veljovic <m.veljovic@levi9.com>
2019-10-17 14:39:52 +0200

    Change title of examples

--- lecture_02/example-02/main.js
+++ lecture_02/example-02/main.js
@@ -1,16 +1,14 @@
 function changeByValue(a) {
   a = 10;
-};
+}
 
 var a = 15;
 changeByValue(a);
 console.log(a);
 
-//
-
 function addToArray(arr) {
   arr.push(10);
-};
+}
 
 var arr = [1, 2, 3];
 addToArray(arr);
