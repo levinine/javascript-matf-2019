0e3c73dde47e3d36a4d3cc9bffe694073b585e27
Vuk Stankovic <v.stankovic@levi9.com>
2019-11-07 11:10:15 +0100

    Callback hell examples

--- lecture_05/example-03/main.js
+++ lecture_05/example-03/main.js
@@ -1,21 +1,15 @@
-function baz() {
-    // call-stack is: 'baz'
-    // so our call-site is in the global scope
-    console.log('baz');
-    bar(); // <-- call-site for bar
-}
-
-function bar() {
-    //call stack is bar -> baz
-    // sou our call site is in baz 
-
-    console.log('bar');
-    foo();
-}
-
-function foo() {
-    // call stack is 'baz' -> 'bar' -> 'foo'
-    // so our call site is in bar
-}
-
-baz(); // <-- call site for 'baz' 
+setTimeout(function () {
+    console.log(1);
+    setTimeout(function () {
+       console.log(2);
+        setTimeout(function () {
+            console.log(3);
+            setTimeout(function () {
+                console.log(4);
+                setTimeout(function () {
+                    console.log(5);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
--- lecture_05/example-04/main.js
+++ lecture_05/example-04/main.js
@@ -1,7 +1,22 @@
-function foo () {
-    console.log(this.a);
+function print(i) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(i);
+            resolve();
+        }, 1000)
+    })
 }
 
-var a = 2;
-
-foo();
+print(1)
+    .then(function () {
+        return print(2);
+    })
+    .then(function () {
+        return print(3);
+    })
+    .then(function () {
+        return print(4);
+    })
+    .then(function () {
+        return print(5);
+    });
