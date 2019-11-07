be967f39fd6d1b568603a5fdafe1d7479651e7c8
Vuk Stankovic <v.stankovic@levi9.com>
2019-11-07 11:39:44 +0100

    Prevent infinite loop

--- lecture_05/example-02/main.js
+++ lecture_05/example-02/main.js
@@ -1,19 +1,22 @@
-var eventLoop = [ ];
-var event;
+function eventLoop() {
+    var eventLoop = [ ];
+    var event;
 
-console.log('Starting event loop');
+    console.log('Starting event loop');
 
-while (true) {
-    if (eventLoop.length > 0) {
-        console.log('Pick first event');
-        event = eventLoop.shift();
+    while (true) {
+        if (eventLoop.length > 0) {
+            console.log('Pick first event');
+            event = eventLoop.shift();
 
-        try {
-            console.log('Try to execute an event');
-            event();
-        }
-        catch (err) {
-            console.log(err);
+            try {
+                console.log('Try to execute an event');
+                event();
+            }
+            catch (err) {
+                console.log(err);
+            }
         }
     }
 }
+
